e32c15fd1f9230f917987848df131e16e142678c
takofaru <fadelior@gmail.com>
2025-06-16 16:11:05 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+    base: "/Kelompok-12-Museum-Nasional",
   root: './src',
   build: {
     outDir: '../dist' // ⬅️ jadi hasil build akan berada di ./dist
